fb1ad2a7f3724c362f8b0069576c82d88cfdf396
Ali Almoullim <Ali.Almoullim@gmail.com>
2019-09-02 02:12:59 +0300

    Allow Significant-change updates (Fixes #3)

--- ios/Classes/SwiftFlutterBackgroundLocationPlugin.swift
+++ ios/Classes/SwiftFlutterBackgroundLocationPlugin.swift
@@ -18,6 +18,10 @@ public class SwiftFlutterBackgroundLocationPlugin: NSObject, FlutterPlugin, CLLo
         SwiftFlutterBackgroundLocationPlugin.locationManager = CLLocationManager()
         SwiftFlutterBackgroundLocationPlugin.locationManager?.delegate = self
         SwiftFlutterBackgroundLocationPlugin.locationManager?.requestAlwaysAuthorization()
+        
+        SwiftBackgroundLocationPlugin.locationManager?.allowsBackgroundLocationUpdates = true
+        SwiftBackgroundLocationPlugin.locationManager?.pausesLocationUpdatesAutomatically = false
+        
         SwiftFlutterBackgroundLocationPlugin.channel?.invokeMethod("location", arguments: "method")
 
         if (call.method == "start_location_service") {
